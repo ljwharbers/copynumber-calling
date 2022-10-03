71507f854206f70d55726d881f6683dfa76ccc65
ljwharbers <l.j.w.harbers@gmail.com>
2022-10-03 15:33:33 +0200

    Added mergeLevels and new integer cn calling

--- cnv-calling/scripts/cnv_calling.R
+++ cnv-calling/scripts/cnv_calling.R
@@ -5,7 +5,7 @@
 ## Script to perform cnv calling from a list of binned read counts
 
 ## Load/install packages
-packages = c("data.table", "argparser", "DNAcopy", "ParDNAcopy", "pbapply", "gtools", "randomForest", "copynumber")
+packages = c("data.table", "argparser", "DNAcopy", "ParDNAcopy", "pbapply", "gtools", "randomForest", "copynumber", "ASCAT.sc", "aCGH")
 invisible(sapply(packages, function(x) suppressMessages(require(x, character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE))))
 
 ## Parse arguments
@@ -30,27 +30,38 @@ parser = add_argument(parser, "--minploidy", help = "Min ploidy of sample (singl
                       nargs = 1, type = "integer", default = 1.5)
 parser = add_argument(parser, "--maxploidy", help = "Max ploidy of sample (single-cell only)", 
                       nargs = 1, type = "integer", default = 6)
+parser = add_argument(parser, "--minpurity", help = "Min purity of sample (1 for single cell / pure cell lines)", 
+                      nargs = 1, type = "integer", default = 1.5)
+parser = add_argument(parser, "--maxpurity", help = "Max purity of sample", 
+                      nargs = 1, type = "integer", default = 6)
+parser = add_argument(parser, "--sex", help = "Sex of sample (either male or female)", nargs = 1, type = "character")
 parser = add_argument(parser, "--threads", help = "Number of threads to use", nargs = 1, type = "integer")
 parser = add_argument(parser, "--output", help = "Path to output file", nargs = 1)
 argv = parse_args(parser)
 
 # argv = list()
-# argv$counts = "/mnt/AchTeraD/data/BICRO291/DG14/cnv/500000/all-counts.tsv.gz"
-# argv$gc = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/GC_variable_500000_150_bwa"
+# argv$counts = "/mnt/sequencing/BICRO328/CD26/cnv/500000/all-counts.tsv.gz"
+# argv$gc = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/GC_variable_500000_76_bwa"
 # argv$blacklist = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/hg19-blacklist.v2_adjusted.bed"
-# argv$bins = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/variable_500000_150_bwa.bed"
-# argv$norm = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/"
+# argv$bins = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/variable_500000_76_bwa.bed"
+# argv$norm = "/mnt/AchTeraD/Documents/Projects/scCUTseq/copynumber-pipeline/cnv-calling/files/hg19/normalize.tsv"
 # argv$binsize = 500000
 # argv$alpha = 0.0001
 # argv$undo.prune = 0.05
-# argv$penalty = 10
+# argv$penalty = 6
 # argv$type = "single"
-# argv$minploidy = 1.5
-# argv$maxploidy = 6
-# argv$threads = 4
-# argv$output = "/mnt/AchTeraD/data/BICRO291/DG14/cnv/500000/cnv.rds"
+# argv$segmentation = "single"
+# argv$minploidy = 1.7
+# argv$maxploidy = 6.5
+# argv$minpurity = 1
+# argv$maxpurity = 1
+# argv$sex = "male"
+# argv$threads = 32
+# argv$output = ""
 # argv$randomforest = "/mnt/AchTeraD/Documents/Projects/scCUTseq/data/cell_classifier/randomforest.rds"
 # argv$rfthreshold = 0.3
+# argv$removethreshold = 300000
+
 
 # Check input parameters
 if(!file.exists(argv$counts)) {
@@ -94,11 +105,25 @@ binsize = argv$binsize
 samples = colnames(out$counts)
 minploidy = argv$minploidy
 maxploidy = argv$maxploidy
+minpurity = argv$minpurity
+maxpurity = argv$maxpurity
 threads = argv$threads
+
+if(argv$sex == "female") {
+  ismale = FALSE
+} else if(argv$sex == "male") {
+  ismale = TRUE
+} else {
+  cat("Error: --sex must be either 'male' or 'female'")
+  stop()
+}
+
 
 output = argv$output
 if(!is.na(argv$randomforest)) rf = readRDS(argv$randomforest)
 rfthreshold = argv$rfthreshold
+
+
 
 ## Define functions
 # LOWESS regression for GC normalization
@@ -133,14 +158,17 @@ out$counts = out$counts[indices]
 out$gc = out$gc[indices]
 out$bins = out$bins[indices]
 
+# Add pseudocount to readcounts
+out$counts = out$counts + 1
+
 # Normalize by mean and GC correction
 cat("Running GC normalization...\n")
 out$counts_gc = out$counts[, pblapply(.SD, function(sample) {
   lowess.gc(out$gc$V1, (sample + 1 / mean(sample + 1)))
-  }, cl = threads)]
+}, cl = threads)]
 
 # Add 0.001 to 0 ratios and make log ratios
-out$counts_gc[out$counts_gc == 0] = 1e-3
+# out$counts_gc[out$counts_gc == 0] = 1e-3
 out$counts_lrr = log2(out$counts_gc)
 
 # Normalize additional segments if requested
@@ -202,12 +230,12 @@ if (out$segmentation_type == "single") {
 if(out$segmentation_type == "joint") {
   # Multipcf
   mpcf_input = as.data.frame(cbind(out$bins[, 1:2], out$counts_lrr))
-  mpcf = multipcf(mpcf_input, gamma = out$penalty, normalize = F)
+  mpcf = multipcf(mpcf_input, gamma = out$penalty, normalize = F, verbose = F)
   
   # Restructure
   out$segments_long = melt(data.table(mpcf), id.vars = c("chrom", "start.pos", "end.pos", "arm", "n.probes"), variable.name = "ID")
-  setnames(out$segments_long, "n.probes", "num.mark")
-  mpcf_rep = out$segments_long[, .(chr = rep(chrom, num.mark), start = rep(start.pos, num.mark), end = rep(end.pos, num.mark), seg.mean = rep(value, num.mark)), by = ID]
+  setnames(out$segments_long, c("chrom", "start.pos", "end.pos", "arm", "num.mark", "ID", "seg.mean"))
+  mpcf_rep = out$segments_long[, .(chr = rep(chrom, num.mark), start = rep(start.pos, num.mark), end = rep(end.pos, num.mark), seg.mean = rep(seg.mean, num.mark)), by = ID]
   
   # Set order
   setorder(mpcf_rep, ID, chr, start)
@@ -218,82 +246,150 @@ if(out$segmentation_type == "joint") {
   setcolorder(out$segments, colnames(out$counts_lrr))
 }
 
-#### If including mergeLevels need to add aCGH package and need to adjust following segments to include merged segments instead of non-merged segments
-# # MergeLevels
-# segments_merged = pblapply(colnames(out$segments), function(cell) {
-#   
-#   # Run mergeLevels and transform to data.table
-#   merged = mergeLevels(out$counts_lrr[[cell]], 
-#                        out$segments[[cell]], 
-#                        pv.thres = .1, 
-#                        verbose = 0)$vecMerged |> data.table()
-#   setnames(merged, cell)
-#   return(merged)
-#   
-# }, cl = threads)
-# out$segments_merged = do.call(cbind, segments_merged)
 
-# Modify log ratio segments to contain median normalized read counts per bin
+#### If including mergeLevels need to add aCGH package and need to adjust following segments to include merged segments instead of non-merged segments
+# MergeLevels
+segments_merged = pblapply(colnames(out$segments), function(cell) {
+
+  # # Run mergeLevels and transform to data.table
+  # merged = mergeLevels(out$counts_lrr[[cell]],
+  #                      out$segments[[cell]],
+  #                      pv.thres = 1e-10,
+  #                      verbose = 0)$vecMerged |> data.table()
+  
+  # Run mergeLevels per chromosome and transform to data.table
+  merged = lapply(unique(out$bins$chr), function(chr) {
+    indices = which(out$bins$chr == chr)
+    mergeLevels(out$counts_lrr[[cell]][indices],
+                out$segments[[cell]][indices],
+                pv.thres = 1e-10,
+                verbose = 0)$vecMerged |> data.table()
+  })
+  merged = rbindlist(merged)
+                  
+  
+  setnames(merged, cell)
+  return(merged)
+
+}, cl = threads)
+out$segments_merged = do.call(cbind, segments_merged)
+
+
+
+# # Modify log ratio segments to contain median normalized read counts per bin
+# median_segments = pblapply(colnames(out$counts_gc), function(cell) {
+#   # Subset segments_long to only have correct sample
+#   dt = data.table(num = out$segments_long[ID == cell]$num.mark)
+#   # Calculate start and end bin for segments
+#   dt[, end := cumsum(num)]
+#   dt[, start := data.table::shift(end, fill = 0) + 1]
+# 
+#   medians = sapply(1:nrow(dt), function(x) {
+#     return(median(out$counts_gc[[cell]][dt[x, start]:dt[x, end]]))
+#   })
+#   return(data.table(rep(medians, dt$num)))
+# }, cl = threads)
+# out$segments_read = do.call(cbind, median_segments)
+# setnames(out$segments_read, colnames(out$counts_gc))
+
+# Modify log ratio segments to contain median normalized read counts per bin 
+#### IN CASE OF MERGELEVELS
 median_segments = pblapply(colnames(out$counts_gc), function(cell) {
-  # Subset segments_long to only have correct sample
-  dt = data.table(num = out$segments_long[ID == cell]$num.mark)
-  # Calculate start and end bin for segments
+  # Get split points
+  splits = c(1, which(out$segments_merged[[cell]] != data.table::shift(out$segments_merged[[cell]])), nrow(out$segments_merged) + 1)
+
+  dt = data.table(num = diff(splits))
   dt[, end := cumsum(num)]
   dt[, start := data.table::shift(end, fill = 0) + 1]
-  
+
+
+
   medians = sapply(1:nrow(dt), function(x) {
     return(median(out$counts_gc[[cell]][dt[x, start]:dt[x, end]]))
   })
-  return(data.table(rep(medians, dt$num)))
+  final = data.table(rep(medians, dt$num))
+  setnames(final, cell)
+
+  return(final)
 }, cl = threads)
+
 out$segments_read = do.call(cbind, median_segments)
 setnames(out$segments_read, colnames(out$counts_gc))
 
 # Renormalize
-out$segments_read = data.table(sweep(out$segments_read, 2, colMeans(out$segments_read), '/'))
+# out$segments_read = data.table(sweep(out$segments_read, 2, colMeans(out$segments_read), '/'))
 
 # Perform integer copy number calling if it's single cell data
 if(type == "single"){
   
-  # Initialize CN inference variables for SoS method -- Code adapted from ginkgo pipeline
-  n_cells = ncol(out$segments_read)
-  ploidy = rbind(c(0,0), c(0,0))
-  CNgrid = seq(minploidy, maxploidy, by = 0.05)
-  n_ploidy = length(CNgrid)  # Number of ploidy tests during CN inference
-  CNmult = matrix(0, n_ploidy, n_cells)
-  CNerror = matrix(0, n_ploidy, n_cells)
-  outerColsums = matrix(0, n_ploidy, n_cells)
-  CN = numeric(n_cells)
+  # # Initialize CN inference variables for SoS method -- Code adapted from ginkgo pipeline
+  # n_cells = ncol(out$segments_read)
+  # ploidy = rbind(c(0,0), c(0,0))
+  # CNgrid = seq(minploidy, maxploidy, by = 0.05)
+  # n_ploidy = length(CNgrid)  # Number of ploidy tests during CN inference
+  # CNmult = matrix(0, n_ploidy, n_cells)
+  # CNerror = matrix(0, n_ploidy, n_cells)
+  # outerColsums = matrix(0, n_ploidy, n_cells)
+  # CN = numeric(n_cells)
+  # 
+  # # Loop through cells and get integer copy number
+  # res = pbsapply(1:n_cells, function(cell) {
+  #   outerRaw = as.matrix(out$segments_read[, cell, with = F]) %o% CNgrid
+  #   outerRound = round(outerRaw)
+  #   outerDiff = (outerRaw - outerRound) ^ 2
+  #   outerColsums[, cell] <<- colSums(outerDiff, na.rm = FALSE, dims = 1) ## Assignment outside scope
+  #   CNmult[, cell] <<- CNgrid[order(outerColsums[, cell])] ## Assignment outside scope
+  #   CNerror[, cell] <<- round(sort(outerColsums[, cell]), digits=2) ## Assignment outside scope
+  #   
+  #   # Define CN multiplier and assign to CN vector
+  #   CN[cell] <<- CNmult[1, cell] ## Assignment outside scope
+  #   # return integer copy number profile
+  #   return(out$segments_read[, cell, with = F] * CN[cell])
+  # })
   
-  # Loop through cells and get integer copy number
-  res = pbsapply(1:n_cells, function(cell) {
-    outerRaw = as.matrix(out$segments_read[, cell, with = F]) %o% CNgrid
-    outerRound = round(outerRaw)
-    outerDiff = (outerRaw - outerRound) ^ 2
-    outerColsums[, cell] <<- colSums(outerDiff, na.rm = FALSE, dims = 1) ## Assignment outside scope
-    CNmult[, cell] <<- CNgrid[order(outerColsums[, cell])] ## Assignment outside scope
-    CNerror[, cell] <<- round(sort(outerColsums[, cell]), digits=2) ## Assignment outside scope
-    
-    # Define CN multiplier and assign to CN vector
-    CN[cell] <<- CNmult[1, cell] ## Assignment outside scope
-    # return integer copy number profile
-    return(out$segments_read[, cell, with = F] * CN[cell])
-  })
+  ploidies = seq(minploidy, maxploidy, .01)
+  purities = seq(minpurity, maxpurity, .01)
   
-  # Setnames to CN
-  names(CN) = colnames(out$segments_read)
-  out$cn = CN
+  # # Loop through segments and get lowest ploidy error
+  # res_ploidies = pblapply(unique(out$segments_long$ID), function(cell) {
+  #   dist_mat = buildDistanceMatrix(out$segments_long[ID == cell, seg.mean], out$segments_long[ID == cell, num.mark], purs = purities, ploidies = ploidies, maxTumourPhi = 8, ismale = ismale)
+  #   mins = arrayInd(which.min(dist_mat), dim(dist_mat))
+  #   ploidy = ploidies[mins[2]]
+  #   purity = purities[mins[1]]
+  # 
+  #   return(data.table(sample = cell,
+  #                     ploidy = ploidy,
+  #                     purity = purity))
+  # }, cl = threads)
+  
+  res_ploidies = pblapply(colnames(out$segments_merged), function(cell) {
+    dist_mat = buildDistanceMatrix(out$segments_merged[[cell]], rep(1, nrow(out$segments_merged)), purs = purities, ploidies = ploidies, maxTumourPhi = 8, ismale = ismale)
+    mins = arrayInd(which.min(dist_mat), dim(dist_mat))
+    ploidy = ploidies[mins[2]]
+    purity = purities[mins[1]]
+
+    return(data.table(sample = cell,
+                      ploidy = ploidy,
+                      purity = purity))
+  }, cl = threads)
+
+  out$ploidies = rbindlist(res_ploidies)
+  # 
+  # # Setnames to CN
+  # names(CN) = colnames(out$segments_read)
+  # out$cn = CN
   
   # Scale counts_lrr
-  res = pblapply(colnames(out$counts_lrr), function(cell) out$counts_lrr[[cell]] * CN[cell], cl = threads)
+  res = pblapply(colnames(out$counts_lrr), function(cell) out$counts_lrr[[cell]] * out$ploidies[sample == cell, ploidy], cl = threads)
   out$counts_lrr_scaled = data.table(do.call(cbind, res))
   setnames(out$counts_lrr_scaled, colnames(out$counts_lrr))
   
   # Save to out
-  res = pblapply(colnames(out$segments_read), function(cell) out$segments_read[[cell]] * CN[cell], cl = threads)
+  res = pblapply(colnames(out$segments_read), function(cell) out$segments_read[[cell]] * out$ploidies[sample == cell, ploidy], cl = threads)
   out$segments_scaled = data.table(do.call(cbind, res))
   setnames(out$segments_scaled, colnames(out$segments_read))
   out$copynumber = round(out$segments_scaled)
+  
   
   # Write stats
   out$stats = data.table(
@@ -310,8 +406,8 @@ if(type == "single"){
   out$stats[, autocorrelation := 
               sapply(sample, function(x) {
                 tail(acf(out$counts_lrr_scaled[[x]], 1, na.action = na.pass, type = "correlation", plot = FALSE)$acf, 1)
-                })
-            ]
+              })
+  ]
   out$stats[, mean_absolute_deviation := sapply(sample, function(x) mad(out$counts_lrr[[x]], constant = 1))]
   
   out$stats[, mean_variance := colMeans(var(out$copynumber), na.rm = TRUE)]
@@ -325,8 +421,8 @@ if(type == "single"){
   out$stats[, mode_cn := 
               sapply(sample, function(x) {
                 names(table(out$copynumber[[x]]))[which(table(out$copynumber[[x]]) == max(table(out$copynumber[[x]])))[1]]
-                })
-            ]
+              })
+  ]
   
   # Run randomforest classification
   prediction = as.data.table(predict(rf, out$stats, type="prob"))
@@ -344,6 +440,7 @@ if(type == "single"){
   out$stats[, coef_variation := sapply(sample, function(x) sd(out$counts_lrr[[x]], na.rm = TRUE) / mean(out$counts_lrr[[x]], na.rm = TRUE))]
   out$stats[, mean_absolute_deviation := sapply(sample, function(x) mad(out$counts_lrr[[x]], constant = 1))]
   out$stats[, breakpoints := out$segments_long[, .N, by = .(ID)]$N - length(unique(out$segments_long$chrom))]
+  
 }
 
 # Write output
